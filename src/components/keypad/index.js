e5a814d3720b988cb27edf22c26120f549d3deed
conrad96 <conrad.mugisha@gmail.com>
2020-08-28 15:37:15 +0300

    added components with respectve scss files

---
+++ src/components/keypad/index.js
@@ -0,0 +1,17 @@
+import react, {Component} from 'react';
+import './keypad.scss';
+
+class KeypadComponent extends Component {
+    constructor(props) {
+        super();
+    }
+    render(props) {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default KeypadComponent;
